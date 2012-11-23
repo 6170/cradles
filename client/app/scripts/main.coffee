af75d7ce12610a73b0a43005d7798c516ece2a72
GeorgeErickson <george55@mit.edu>
2012-11-23 17:58:32 -0500

    rails setup with pg

--- client/app/scripts/main.coffee
+++
@@ -1,9 +0,0 @@
-require.config
-  shim: {}
-  paths:
-    jquery: "vendor/jquery.min"
-
-require ["app"], (app) ->
-  
-  # use app here
-  console.log app
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cradles::Application.config.secret_token = '43f7547a2a3bb31fcfb565866ab96b12a31a051031b90ed0706daccc3fa7b2d76ed85f8a9f187e9f1c32a25df5935d31a7e5f078bf82dc32217cbe3713a040c4'
