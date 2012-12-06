b2c874cb854bbbade860934e86ecb8609cffcd01
David Benhaim <davidbenhaim12@gmail.com>
2012-12-05 20:28:37 -0500

    fixing bug

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,15 @@
 class NotificationMailer < ActionMailer::Base
   default from: "from@example.com"
   def notification(message)
-    @from = "no-reply@cradles.herokuapps.com"
-    @recipients = message.to     
+  	@message = message
+  	@from = "no-reply@cradles.herokuapps.com"
+    @recipients = @message.to     
     @subject = "#{message.to} has sent you a message on VolunteerNet"     
    	@message = message
 	@sent_on = Time.now
 	@headers = {}
+  	mail(:to => @message.to) do |format|
+      format.html
+    end
   end
 end
