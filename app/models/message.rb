6b32d8d7e947211b75d020764f17bbaf2384b68a
David Benhaim <davidbenhaim12@gmail.com>
2012-12-05 19:52:14 -0500

    fixing bug

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,9 +2,9 @@ class Message < ActiveRecord::Base
   attr_accessible :content, :from, :to, :conversation_id, :was_read
   validate :content, :on => :create
   belongs_to :conversation
-  after_save :notify
+  after_create :notify
 
   def notify
-    NotificationMailer::deliver_message_notification(self)
+    NotificationMailer::deliver_notification(self)
   end
 end
